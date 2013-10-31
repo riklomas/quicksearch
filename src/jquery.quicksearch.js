6130c9f6fd6215cefd51b97ad25cb11b1f85953e
Nicolas Brassard <nico@nitriques.com>
2013-10-31 14:44:15 -0400

    code clean up and some refactor; public defaults

--- src/jquery.quicksearch.js
+++ src/jquery.quicksearch.js
@@ -6,6 +6,42 @@
 (function($, window, document, undefined) {
 	'use strict';
 	
+	$.quicksearch = {
+		defaults: { 
+			delay: 100,
+			selector: null,
+			stripeRows: null,
+			loader: null,
+			noResults: '',
+			matchedResultsCount: 0,
+			bind: 'keyup',
+			removeDiacritics: false,
+			onBefore: function () { 
+				return;
+			},
+			onAfter: function () { 
+				return;
+			},
+			show: function () {
+				this.style.display = "";
+			},
+			hide: function () {
+				this.style.display = "none";
+			},
+			prepareQuery: function (val) {
+				return val.toLowerCase().split(' ');
+			},
+			testQuery: function (query, txt, _row) {
+				for (var i = 0; i < query.length; i += 1) {
+					if (txt.indexOf(query[i]) === -1) {
+						return false;
+					}
+				}
+				return true;
+			}
+		}
+	};
+	
 	$.fn.quicksearch = function (target, opt) {
 		
 		var defaultDiacriticsRemovalMap = [
@@ -103,38 +139,9 @@
 			return str;
 		};
 		
-		var timeout, cache, rowcache, jq_results, val = '', e = this, options = $.extend({ 
-			delay: 100,
-			selector: null,
-			stripeRows: null,
-			loader: null,
-			noResults: '',
-			matchedResultsCount: 0,
-			bind: 'keyup',
-			onBefore: function () { 
-				return;
-			},
-			onAfter: function () { 
-				return;
-			},
-			show: function () {
-				this.style.display = "";
-			},
-			hide: function () {
-				this.style.display = "none";
-			},
-			prepareQuery: function (val) {
-				return e.removeDiacritics(val).toLowerCase().split(' ');
-			},
-			testQuery: function (query, txt, _row) {
-				for (var i = 0; i < query.length; i += 1) {
-					if (txt.indexOf(query[i]) === -1) {
-						return false;
-					}
-				}
-				return true;
-			}
-		}, opt);
+		var timeout, cache, rowcache, jq_results, val = '', 
+			self = this, 
+			options = $.extend($.quicksearch.defaults, opt);
 		
 		this.go = function () {
 			
@@ -142,8 +149,14 @@
 				len = 0,
 				numMatchedRows = 0,
 				noresults = true, 
-				query = options.prepareQuery(val),
+				query,
 				val_empty = (val.replace(' ', '').length === 0);
+				
+			if (options.removeDiacritics) {
+				val = self.removeDiacritics(val);
+			}
+			
+			query = options.prepareQuery(val);
 			
 			for (i = 0, len = rowcache.length; i < len; i++) {
 				if (val_empty || options.testQuery(query, cache[i], rowcache[i])) {
@@ -173,7 +186,7 @@
 		 * */
 		this.search = function (submittedVal) {
 			val = submittedVal;
-			e.trigger();
+			self.trigger();
 		};
 		
 		/*
@@ -186,8 +199,7 @@
 		
 		this.stripe = function () {
 			
-			if (typeof options.stripeRows === "object" && options.stripeRows !== null)
-			{
+			if (typeof options.stripeRows === "object" && options.stripeRows !== null) {
 				var joined = options.stripeRows.join(' ');
 				var stripeRows_length = options.stripeRows.length;
 				
@@ -245,13 +257,13 @@
 			var t = (typeof options.selector === "string") ? jq_results.find(options.selector) : $(target).not(options.noResults);
 						
 			cache = t.map(function () {
-				return e.removeDiacritics(e.strip_html(this.innerHTML));
+				return self.removeDiacritics(self.strip_html(this.innerHTML));
 			});
 			
 			rowcache = jq_results.map(function () {
 				return this;
 			});
-
+			
 			/*
 			 * Modified fix for sync-ing "val". 
 			 * Original fix https://github.com/michaellwest/quicksearch/commit/4ace4008d079298a01f97f885ba8fa956a9703d1
@@ -271,7 +283,7 @@
 			
 			window.clearTimeout(timeout);
 			timeout = window.setTimeout(function () {
-				e.go();
+				self.go();
 			}, options.delay);
 			
 			return this;
@@ -290,7 +302,7 @@
 			$(this).on(options.bind, function () {
 				
 				val = $(this).val();
-				e.trigger();
+				self.trigger();
 			});
 		});
 		
